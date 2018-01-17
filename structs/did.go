b699034d244603c043be4142c4056b25a2a72589
wangxiaohui <wangxiaohui@arxanfintech.com>
2018-01-17 18:26:34 +0800

    [T2524] Refine did type definitions
    Change-Id: Iace5470efc6da9185d10c69578e5ff2a41e8d44c

---
+++ structs/did.go
@@ -0,0 +1,52 @@
+/*
+Copyright ArxanFintech Technology Ltd. 2018 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+                 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package structs
+
+type (
+	// IDIdentity format "did:<provider short id>:id_string"
+	// IVerifiableClaim format "vcm:<adapter shrot id>:id_string"
+	Identifier  string
+	DidEndpoint string
+	DidContext  string
+)
+
+// Did type define
+type DidType string
+
+const (
+	// Common did type (Main wallet type)
+	DTAsset        DidType = "Asset"
+	DTEntity       DidType = "Entity"
+	DTOrganization DidType = "Organization"
+	DTPerson       DidType = "Person"      //Individual
+	DTIndependent  DidType = "Independent" //Independent person
+	DTDependent    DidType = "Dependent"   //Dependent person
+
+	// Sub wallet did type
+	DTCash     DidType = "cash"
+	DTFee      DidType = "fee"
+	DTLoan     DidType = "loan"
+	DTInterest DidType = "interest"
+)
+
+// Did status define
+type DidStatus string
+
+const (
+	DSValid   DidStatus = "Valid"
+	DSInvalid DidStatus = "Invalid"
+)
